ff273e7fc5c79d92080ed42e83c3649aa6cd9352
Mudit Garg <70060147+MG2708@users.noreply.github.com>
2021-10-04 11:41:39 +0530

    Delete mysql db connection create table.py

--- CS Project 2020-21 Bundling/mysql db connection create table.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host='156.67.222.190',user='u518149110_admin',password='Ke460qIg~4t')
-mycursor=mydb.cursor()
-mycursor.execute("Use u518149110_atm")
-mycursor.execute("Create Table users(Username varchar(6) Primary Key,Pin int(4))")
-for x in mycursor:
-    print(x)
